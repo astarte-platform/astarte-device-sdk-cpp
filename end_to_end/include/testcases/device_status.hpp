395db00421f707f970f4274fb28803f7656511b2
Riccardo Gallo <riccardo.gallo@secomind.com>
2025-06-09 14:57:50 +0200

    refactor(test): split e2e test cases into separate files
    Inprove extensibility and maintainability by splitting the end-to-end
    test cases into separate files.
    Also use string_view instead of string in the AstarteMesage constructor
    to pass the interface name, this way it is possible to pass a string literal
    without the need to create a temporary string object (useful when testing).
    
    Signed-off-by: Riccardo Gallo <riccardo.gallo@secomind.com>

---
+++ end_to_end/include/testcases/device_status.hpp
@@ -0,0 +1,27 @@
+// (C) Copyright 2025, SECO Mind Srl
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#include "case.hpp"
+#include "constants/astarte_interfaces.hpp"
+
+namespace testcases {
+TestCase device_status() {
+ return TestCase(
+      "Device status",
+      std::vector<std::shared_ptr<TestAction>>{
+          std::make_shared<TestActionConnect>(),
+          std::make_shared<TestActionSleep>(std::chrono::seconds(1)),
+          std::make_shared<TestActionCheckDeviceStatus>(
+              true, std::vector<std::string>{std::string(astarte_interfaces::DEVICE_DATASTREAM),
+                                             std::string(astarte_interfaces::SERVER_DATASTREAM),
+                                             std::string(astarte_interfaces::DEVICE_AGGREGATE),
+                                             std::string(astarte_interfaces::SERVER_AGGREGATE),
+                                             std::string(astarte_interfaces::DEVICE_PROPERTY),
+                                             std::string(astarte_interfaces::SERVER_PROPERTY)}),
+          std::make_shared<TestActionDisconnect>(),
+          std::make_shared<TestActionSleep>(std::chrono::seconds(1))});
+}
+}  // namespace testcases
---
+++ end_to_end/include/testcases/server_datastream.hpp
@@ -0,0 +1,148 @@
+// (C) Copyright 2025, SECO Mind Srl
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#include "case.hpp"
+#include "constants/astarte_interfaces.hpp"
+
+using AstarteDeviceSdk::AstarteMessageType;
+
+namespace testcases {
+TestCase server_datastream() {
+  return TestCase(
+      "Astarte to device",
+      std::vector<std::shared_ptr<TestAction>>{
+          std::make_shared<TestActionConnect>(),
+          std::make_shared<TestActionSleep>(std::chrono::seconds(1)),
+          std::make_shared<TestActionTransmitRESTData>(
+              AstarteMessage(astarte_interfaces::SERVER_DATASTREAM, "/integer_endpoint",
+                             AstarteMessageType::DATASTREAM_INDIVIDUAL, AstarteData((int32_t)43))),
+          std::make_shared<TestActionTransmitRESTData>(AstarteMessage(
+              astarte_interfaces::SERVER_DATASTREAM, "/longinteger_endpoint",
+              AstarteMessageType::DATASTREAM_INDIVIDUAL, AstarteData((int64_t)17179869184))),
+          std::make_shared<TestActionTransmitRESTData>(
+              AstarteMessage(astarte_interfaces::SERVER_DATASTREAM, "/double_endpoint",
+                             AstarteMessageType::DATASTREAM_INDIVIDUAL, AstarteData(4245.23))),
+          std::make_shared<TestActionTransmitRESTData>(
+              AstarteMessage(astarte_interfaces::SERVER_DATASTREAM, "/boolean_endpoint",
+                             AstarteMessageType::DATASTREAM_INDIVIDUAL, AstarteData(false))),
+          std::make_shared<TestActionTransmitRESTData>(AstarteMessage(
+              astarte_interfaces::SERVER_DATASTREAM, "/string_endpoint",
+              AstarteMessageType::DATASTREAM_INDIVIDUAL, AstarteData(std::string("Hello world")))),
+          std::make_shared<TestActionTransmitRESTData>(AstarteMessage(
+              astarte_interfaces::SERVER_DATASTREAM, "/datetime_endpoint",
+              AstarteMessageType::DATASTREAM_INDIVIDUAL,
+              AstarteData(std::chrono::system_clock::time_point(
+                  std::chrono::sys_days(std::chrono::year_month_day(
+                      std::chrono::year(1994), std::chrono::month(4), std::chrono::day(12))) +
+                  std::chrono::hours(10) + std::chrono::minutes(15) + std::chrono::seconds(0))))),
+          std::make_shared<TestActionTransmitRESTData>(
+              AstarteMessage(astarte_interfaces::SERVER_DATASTREAM, "/binaryblob_endpoint",
+                             AstarteMessageType::DATASTREAM_INDIVIDUAL,
+                             AstarteData(std::vector<uint8_t>{0x23, 0x43, 0xF5}))),
+          std::make_shared<TestActionTransmitRESTData>(
+              AstarteMessage(astarte_interfaces::SERVER_DATASTREAM, "/integerarray_endpoint",
+                             AstarteMessageType::DATASTREAM_INDIVIDUAL,
+                             AstarteData(std::vector<int32_t>{32, 43, 0, 3332}))),
+          std::make_shared<TestActionTransmitRESTData>(
+              AstarteMessage(astarte_interfaces::SERVER_DATASTREAM, "/longintegerarray_endpoint",
+                             AstarteMessageType::DATASTREAM_INDIVIDUAL,
+                             AstarteData(std::vector<int64_t>{17179869184, 0, 6486}))),
+          std::make_shared<TestActionTransmitRESTData>(
+              AstarteMessage(astarte_interfaces::SERVER_DATASTREAM, "/doublearray_endpoint",
+                             AstarteMessageType::DATASTREAM_INDIVIDUAL,
+                             AstarteData(std::vector<double>{0.0, 23.2}))),
+          std::make_shared<TestActionTransmitRESTData>(
+              AstarteMessage(astarte_interfaces::SERVER_DATASTREAM, "/booleanarray_endpoint",
+                             AstarteMessageType::DATASTREAM_INDIVIDUAL,
+                             AstarteData(std::vector<bool>{false, true, true}))),
+          std::make_shared<TestActionTransmitRESTData>(AstarteMessage(
+              astarte_interfaces::SERVER_DATASTREAM, "/stringarray_endpoint",
+              AstarteMessageType::DATASTREAM_INDIVIDUAL,
+              AstarteData(std::vector<std::string>{"Hello ", "world ", "from ", "C++"}))),
+          std::make_shared<TestActionTransmitRESTData>(AstarteMessage(
+              astarte_interfaces::SERVER_DATASTREAM, "/datetimearray_endpoint",
+              AstarteMessageType::DATASTREAM_INDIVIDUAL,
+              AstarteData(std::vector<std::chrono::system_clock::time_point>{
+                  std::chrono::sys_days(std::chrono::year_month_day(
+                      std::chrono::year(1994), std::chrono::month(4), std::chrono::day(12))) +
+                      std::chrono::hours(10) + std::chrono::minutes(15) + std::chrono::seconds(0),
+                  std::chrono::sys_days(std::chrono::year_month_day(
+                      std::chrono::year(1985), std::chrono::month(5), std::chrono::day(22))) +
+                      std::chrono::hours(0) + std::chrono::minutes(0) + std::chrono::seconds(12),
+              }))),
+          std::make_shared<TestActionTransmitRESTData>(
+              AstarteMessage(astarte_interfaces::SERVER_DATASTREAM, "/binaryblobarray_endpoint",
+                             AstarteMessageType::DATASTREAM_INDIVIDUAL,
+                             AstarteData(std::vector<std::vector<uint8_t>>{{0x23, 0x43, 0xF5},
+                                                                           {0x43, 0xF3, 0x00}}))),
+          std::make_shared<TestActionSleep>(std::chrono::seconds(1)),
+          std::make_shared<TestActionReadReceivedMQTTData>(
+              AstarteMessage(astarte_interfaces::SERVER_DATASTREAM, "/integer_endpoint",
+                             AstarteMessageType::DATASTREAM_INDIVIDUAL, AstarteData((int32_t)43))),
+          std::make_shared<TestActionReadReceivedMQTTData>(AstarteMessage(
+              astarte_interfaces::SERVER_DATASTREAM, "/longinteger_endpoint",
+              AstarteMessageType::DATASTREAM_INDIVIDUAL, AstarteData((int64_t)17179869184))),
+          std::make_shared<TestActionReadReceivedMQTTData>(
+              AstarteMessage(astarte_interfaces::SERVER_DATASTREAM, "/double_endpoint",
+                             AstarteMessageType::DATASTREAM_INDIVIDUAL, AstarteData(4245.23))),
+          std::make_shared<TestActionReadReceivedMQTTData>(
+              AstarteMessage(astarte_interfaces::SERVER_DATASTREAM, "/boolean_endpoint",
+                             AstarteMessageType::DATASTREAM_INDIVIDUAL, AstarteData(false))),
+          std::make_shared<TestActionReadReceivedMQTTData>(AstarteMessage(
+              astarte_interfaces::SERVER_DATASTREAM, "/string_endpoint",
+              AstarteMessageType::DATASTREAM_INDIVIDUAL, AstarteData(std::string("Hello world")))),
+          std::make_shared<TestActionReadReceivedMQTTData>(AstarteMessage(
+              astarte_interfaces::SERVER_DATASTREAM, "/datetime_endpoint",
+              AstarteMessageType::DATASTREAM_INDIVIDUAL,
+              AstarteData(std::chrono::system_clock::time_point(
+                  std::chrono::sys_days(std::chrono::year_month_day(
+                      std::chrono::year(1994), std::chrono::month(4), std::chrono::day(12))) +
+                  std::chrono::hours(10) + std::chrono::minutes(15) + std::chrono::seconds(0))))),
+          std::make_shared<TestActionReadReceivedMQTTData>(
+              AstarteMessage(astarte_interfaces::SERVER_DATASTREAM, "/binaryblob_endpoint",
+                             AstarteMessageType::DATASTREAM_INDIVIDUAL,
+                             AstarteData(std::vector<uint8_t>{0x23, 0x43, 0xF5}))),
+          std::make_shared<TestActionReadReceivedMQTTData>(
+              AstarteMessage(astarte_interfaces::SERVER_DATASTREAM, "/integerarray_endpoint",
+                             AstarteMessageType::DATASTREAM_INDIVIDUAL,
+                             AstarteData(std::vector<int32_t>{32, 43, 0, 3332}))),
+          std::make_shared<TestActionReadReceivedMQTTData>(
+              AstarteMessage(astarte_interfaces::SERVER_DATASTREAM, "/longintegerarray_endpoint",
+                             AstarteMessageType::DATASTREAM_INDIVIDUAL,
+                             AstarteData(std::vector<int64_t>{17179869184, 0, 6486}))),
+          std::make_shared<TestActionReadReceivedMQTTData>(
+              AstarteMessage(astarte_interfaces::SERVER_DATASTREAM, "/doublearray_endpoint",
+                             AstarteMessageType::DATASTREAM_INDIVIDUAL,
+                             AstarteData(std::vector<double>{0.0, 23.2}))),
+          std::make_shared<TestActionReadReceivedMQTTData>(
+              AstarteMessage(astarte_interfaces::SERVER_DATASTREAM, "/booleanarray_endpoint",
+                             AstarteMessageType::DATASTREAM_INDIVIDUAL,
+                             AstarteData(std::vector<bool>{false, true, true}))),
+          std::make_shared<TestActionReadReceivedMQTTData>(AstarteMessage(
+              astarte_interfaces::SERVER_DATASTREAM, "/stringarray_endpoint",
+              AstarteMessageType::DATASTREAM_INDIVIDUAL,
+              AstarteData(std::vector<std::string>{"Hello ", "world ", "from ", "C++"}))),
+          std::make_shared<TestActionReadReceivedMQTTData>(AstarteMessage(
+              astarte_interfaces::SERVER_DATASTREAM, "/datetimearray_endpoint",
+              AstarteMessageType::DATASTREAM_INDIVIDUAL,
+              AstarteData(std::vector<std::chrono::system_clock::time_point>{
+                  std::chrono::sys_days(std::chrono::year_month_day(
+                      std::chrono::year(1994), std::chrono::month(4), std::chrono::day(12))) +
+                      std::chrono::hours(10) + std::chrono::minutes(15) + std::chrono::seconds(0),
+                  std::chrono::sys_days(std::chrono::year_month_day(
+                      std::chrono::year(1985), std::chrono::month(5), std::chrono::day(22))) +
+                      std::chrono::hours(0) + std::chrono::minutes(0) + std::chrono::seconds(12),
+              }))),
+          std::make_shared<TestActionTransmitRESTData>(
+              AstarteMessage(astarte_interfaces::SERVER_DATASTREAM, "/binaryblobarray_endpoint",
+                             AstarteMessageType::DATASTREAM_INDIVIDUAL,
+                             AstarteData(std::vector<std::vector<uint8_t>>{{0x23, 0x43, 0xF5},
+                                                                           {0x43, 0xF3, 0x00}}))),
+          std::make_shared<TestActionSleep>(std::chrono::seconds(1)),
+          std::make_shared<TestActionDisconnect>(),
+          std::make_shared<TestActionSleep>(std::chrono::seconds(1))});
+}
+}  // namespace testcases
