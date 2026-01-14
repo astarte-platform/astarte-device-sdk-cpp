8848e2a6de7eb151fc18f230bfdc303066bdccc5
Simone Orru <simone.orru@secomind.com>
2026-01-14 17:53:24 +0100

    Enable a check device status test for MQTT
    Signed-off-by: Simone Orru <simone.orru@secomind.com>

--- end_to_end/src/main.cpp
+++ end_to_end/src/main.cpp
@@ -67,12 +67,20 @@ bool validate_config(const toml::table& config) {
     spdlog::error("Configuration Error: Missing '[mqtt]' table.");
     is_valid = false;
   } else {
+    if (!config["mqtt"]["store_dir"].is_string()) {
+      spdlog::error("Configuration Error: Missing 'store_dir' inside [mqtt] table.");
+      is_valid = false;
+    }
     if (!config["mqtt"]["pairing_token"].is_string()) {
       spdlog::error("Configuration Error: Missing 'pairing_token' inside [mqtt] table.");
       is_valid = false;
     }
     if (!config["mqtt"]["device_id_registration"].is_string()) {
       spdlog::error("Configuration Error: Missing 'device_id_registration' inside [mqtt] table.");
+      is_valid = false;
+    }
+    if (!config["mqtt"]["credential_secret"].is_string()) {
+      spdlog::error("Configuration Error: Missing 'credential_secret' inside [mqtt] table.");
       is_valid = false;
     }
   }
@@ -141,28 +149,27 @@ int main() {
 
   device_factory = std::make_shared<TestGrpcDeviceFactory>(grpc_conf);
 #else
+  auto store_dir = config["mqtt"]["store_dir"].value<std::string>().value();
   auto pairing_token_opt = config.at("mqtt").at_path("pairing_token").value<std::string>();
   auto device_id_registration_opt =
       config.at("mqtt").at_path("device_id_registration").value<std::string>();
+  auto credential_secret_opt = config.at("mqtt").at_path("credential_secret").value<std::string>();
 
-  // TODO(sorru): enable when at least one test will use this
-  // auto store_dir = config["mqtt"]["store_dir"].value<std::string>().value();
-  // TestMqttDeviceConfig mqtt_conf = {
-  //     .realm = realm,
-  //     .device_id = device_id,
-  //     .credential_secret = credential_secret_opt.value(),
-  //     .pairing_url = astarte_fmt::format("{}/pairing", astarte_base_url),
-  //     .store_dir = store_dir,
-  //     .interfaces = {
-  //         astarte_interfaces::DeviceDatastream::FILE,
-  //         astarte_interfaces::ServerDatastream::FILE,
-  //         astarte_interfaces::DeviceAggregate::FILE,
-  //         astarte_interfaces::ServerAggregate::FILE,
-  //         astarte_interfaces::DeviceProperty::FILE,
-  //         astarte_interfaces::ServerProperty::FILE,
-  //     }};
-  // device_factory = std::make_shared<TestMqttDeviceFactory>(std::move(mqtt_conf));
-
+  TestMqttDeviceConfig mqtt_conf = {
+      .realm = realm,
+      .device_id = device_id,
+      .credential_secret = credential_secret_opt.value(),
+      .pairing_url = astarte_fmt::format("{}/pairing", astarte_base_url),
+      .store_dir = store_dir,
+      .interfaces = {
+          astarte_interfaces::DeviceDatastream::FILE,
+          astarte_interfaces::ServerDatastream::FILE,
+          astarte_interfaces::DeviceAggregate::FILE,
+          astarte_interfaces::ServerAggregate::FILE,
+          astarte_interfaces::DeviceProperty::FILE,
+          astarte_interfaces::ServerProperty::FILE,
+      }};
+  device_factory = std::make_shared<TestMqttDeviceFactory>(std::move(mqtt_conf));
 #endif
 
   TestOrchestrator orchestrator(config_http, device_factory);
@@ -173,7 +180,9 @@ int main() {
   orchestrator.add_test_case(
       testcases::device_pairing(pairing_token_opt.value(), device_id_registration_opt.value()));
   // Standard tests using existing credentials (device functionality not yet implemented)
+  orchestrator.add_test_case(testcases::device_status(device_id));
   // register_standard_test_suite(orchestrator);
+
 #endif
   orchestrator.execute_all();
 
